f9f189e17ac05c551fa4011a843f7cd0012315b5
Manonmaie <manonmaiepasumarthy@gmail.com>
2020-05-04 20:36:07 +0530

    Initial Commit

---
+++ src/main/java/com/calculator/Calculator.java
@@ -0,0 +1,8 @@
+class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Sum is:"+ add(3,4));
+    }
+    public static int add(int a, int b) {
+    	return a+b;
+    }
+}
